930d405e14558dcbba634640f02a5e9b66da3a14
Chris Lemke <spcbrn1@gmail.com>
2018-02-02 11:13:59 -0700

    comment out unused code, comments

--- src/App/SigBox.js
+++ src/App/SigBox.js
@@ -133,6 +133,7 @@ class SigBox extends Component {
         <button onClick={() => this.writeSignature(dummySig())}>Print Demo</button><br /><br /><br />
 
         <p>Enter URI to POST current image to (default '/api/signature/new'):</p><br />
+        <p><small>{ `*if using default path, each POST will return the list of current entries, logged to the console`}</small></p><br />
         <p><small>{ `Request Body: { id: null, data: STRING (base64 canvas image data)}`}</small></p>
         <input ref="post_uri" placeholder="http://yourserver/post/endpoint" />
         <button onClick={this.captureCanvas}>Send</button><br /><br /><br />
@@ -152,53 +153,53 @@ export default SigBox;
 
 
 
-const Bezier = (startPoint, control1, control2, endPoint) => {
-  this.startPoint = startPoint;
-  this.control1 = control1;
-  this.control2 = control2;
-  this.endPoint = endPoint;
-}
-
-// Returns approximated length.
-Bezier.prototype.length = function () {
-  const steps = 10;
-  let length = 0;
-  let px;
-  let py;
-
-  for (let i = 0; i <= steps; i += 1) {
-    const t = i / steps;
-    const cx = this._point(
-      t,
-      this.startPoint.x,
-      this.control1.x,
-      this.control2.x,
-      this.endPoint.x,
-    );
-    const cy = this._point(
-      t,
-      this.startPoint.y,
-      this.control1.y,
-      this.control2.y,
-      this.endPoint.y,
-    );
-    if (i > 0) {
-      const xdiff = cx - px;
-      const ydiff = cy - py;
-      length += Math.sqrt((xdiff * xdiff) + (ydiff * ydiff));
-    }
-    px = cx;
-    py = cy;
-  }
-
-  return length;
-};
-
-/* eslint-disable no-multi-spaces, space-in-parens */
-Bezier.prototype._point = function (t, start, c1, c2, end) {
-  return (       start * (1.0 - t) * (1.0 - t)  * (1.0 - t))
-       + (3.0 *  c1    * (1.0 - t) * (1.0 - t)  * t)
-       + (3.0 *  c2    * (1.0 - t) * t          * t)
-       + (       end   * t         * t          * t);
-};
-/* eslint-enable no-multi-spaces, space-in-parens */
+// const Bezier = (startPoint, control1, control2, endPoint) => {
+//   this.startPoint = startPoint;
+//   this.control1 = control1;
+//   this.control2 = control2;
+//   this.endPoint = endPoint;
+// }
+//
+// // Returns approximated length.
+// Bezier.prototype.length = function () {
+//   const steps = 10;
+//   let length = 0;
+//   let px;
+//   let py;
+//
+//   for (let i = 0; i <= steps; i += 1) {
+//     const t = i / steps;
+//     const cx = this._point(
+//       t,
+//       this.startPoint.x,
+//       this.control1.x,
+//       this.control2.x,
+//       this.endPoint.x,
+//     );
+//     const cy = this._point(
+//       t,
+//       this.startPoint.y,
+//       this.control1.y,
+//       this.control2.y,
+//       this.endPoint.y,
+//     );
+//     if (i > 0) {
+//       const xdiff = cx - px;
+//       const ydiff = cy - py;
+//       length += Math.sqrt((xdiff * xdiff) + (ydiff * ydiff));
+//     }
+//     px = cx;
+//     py = cy;
+//   }
+//
+//   return length;
+// };
+//
+// /* eslint-disable no-multi-spaces, space-in-parens */
+// Bezier.prototype._point = function (t, start, c1, c2, end) {
+//   return (       start * (1.0 - t) * (1.0 - t)  * (1.0 - t))
+//        + (3.0 *  c1    * (1.0 - t) * (1.0 - t)  * t)
+//        + (3.0 *  c2    * (1.0 - t) * t          * t)
+//        + (       end   * t         * t          * t);
+// };
+// /* eslint-enable no-multi-spaces, space-in-parens */
