266b62dc5569d23c349b6106e9f61bfac36761b5
Chris Lemke <spcbrn1@gmail.com>
2018-02-02 10:02:03 -0700

    basic demo running

--- src/App/App.js
+++ src/App/App.js
@@ -12,13 +12,13 @@ class App extends Component {
     return (
       <main className="demo_main">
         <h4>{`Custom React signature box (mouse/touch):`}</h4>
-        <p>{`(First prototype, utterly basic.)`}</p>
+        <p>{`First prototype, custom React component using canvas, no math yet, utterly basic.`}</p>
 
           <SigBox />
 
         <div className="vert_divider"></div>
         <h4>{`Open-source React signature box (mouse/touch):`}</h4>
-        <p>{`(Integrated throttling, Bezier curve function, and velocity based point thickness.)`}</p>
+        <p>{`Integrated throttling, Bezier curve function, and velocity based point thickness.  #goals`}</p>
 
           <SignatureCanvas canvasProps={{ width: 450, height: 200, className: "r-s-canvas"}} />
 
